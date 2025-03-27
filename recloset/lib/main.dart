8b1ce6dc8bf3e3ce76ab514e81c3fe5c1330a9c4
woojin-devv <dnwls0723@sookmyung.ac.kr>
2025-03-28 00:49:22 +0900

    feat: Add font package 'google outfit'

--- recloset/lib/main.dart
+++ recloset/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/intro.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: IntroScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.outfitTextTheme(),
+      ),
+      home: const IntroScreen(),
     );
   }
 }
